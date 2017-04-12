24d9d7a747850e867059192a8c9857ae6b8c2391
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2017-04-12 17:16:30 -0300

    Testes: Número 03, Argumento inválido

--- src/TestEnvironment.c
+++ src/TestEnvironment.c
@@ -3,7 +3,7 @@
 #include "gtest/gtest.h"
 #include "RomanNumberConversion.h"
 
-char one[30] = "I", two[30] = "II";
+char one[30] = "I", two[30] = "II", three[30] = "III", invalid[30] = "A";
 
 TEST (romanNumberConversion, I) {
 
@@ -14,6 +14,18 @@ TEST (romanNumberConversion, I) {
 TEST (romanNumberConversion, II) {
 
     EXPECT_EQ ((romanNumberConversion(two) == 2), 1);
+
+}
+
+TEST (romanNumberConversion, III) {
+
+    EXPECT_EQ ((romanNumberConversion(three) == 3), 1);
+
+}
+
+TEST (romanNumberConversion, invalidArgument) {
+
+    EXPECT_EQ ((romanNumberConversion(invalid) == -1), 1);
 
 }
 
